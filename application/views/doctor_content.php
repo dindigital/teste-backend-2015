ba7139270724038dfed4ffa92e296315d8db4a83
Marcelo de Oliveira Lopes <mol360@gmail.com>
2015-10-21 00:07:09 -0200

    * Implementação de framework codigniter. * Adição de páginas(views!).

---
+++ application/views/doctor_content.php
@@ -0,0 +1,16 @@
+<!-- intro close -->
+<section id="about" class="about-section"><!-- about start -->
+	<section id="doctor">
+	  <div class="container">
+	    <div class="row">
+	      <div class="col-md-7">
+	        <h1>Nome do Doutor</h1>
+	        <h1>Especialidade</h1>
+	        <p class="lead">Descrição</p>      
+	      </div>
+	    </div>
+	  </div>
+	</section>
+</section>
+<!-- about close -->
+<!-- service close -->
---
+++ application/views/footer.php
@@ -0,0 +1,34 @@
+<div class="footer"><!-- footer section start -->
+  <div class="container">
+    <div class="row ft">
+      <div class="col-md-9">
+        <p> © Copyright 2014. All Rights Reserved by Wealth.life </p>
+      </div>
+      <div class="col-md-3"><p> <a href="#"> <i class="fa fa-facebook-square fa-size social-icon"> </i></a>
+      <a href="#"> <i class="fa  fa-twitter-square  fa-size social-icon"> </i> </a>
+      <a href="#"><i class="fa fa-google-plus-square fa-size social-icon"> </i></a> 
+      <a href="#"><i class="fa fa-flickr fa-size social-icon"> </i></a> 
+      <a href="#"><i class="fa fa-pinterest-square fa-size social-icon"> </i> </a></p></div>
+    </div>
+  </div>
+</div><!-- footer section close -->
+
+<!-- Core JavaScript Files --> 
+<script src="<?php base_url(); ?>includes/js/jquery-1.10.2.js"></script> 
+<script src="<?php base_url(); ?>includes/js/bootstrap.min.js"></script> 
+<script src="<?php base_url(); ?>includes/js/jquery.easing.min.js"></script> 
+<!-- Custom Theme JavaScript --> 
+<script src="<?php base_url(); ?>includes/js/scrolling-nav.js"></script> 
+<script src="<?php base_url(); ?>includes/js/owl.carousel.js"></script> 
+
+<script>
+  $("#owl-demo").owlCarousel({
+    autoPlay: 3000, //Set AutoPlay to 3 seconds
+    items : 4,
+    itemsDesktop : [1199,3],
+    itemsDesktopSmall : [979,3]
+  });
+</script>
+
+</body>
+</html>
