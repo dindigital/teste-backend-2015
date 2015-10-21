4d16b70b97f92be14e037103b30224d472e6e31a
Marcelo de Oliveira Lopes <mol360@gmail.com>
2015-10-21 02:57:54 -0200

    * Ajuste para retirar index.php da url.

--- application/config/config.php
+++ application/config/config.php
@@ -504,4 +504,4 @@ $config['rewrite_short_tags'] = FALSE;
 | Comma-separated:	'10.0.1.200,192.168.5.0/24'
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
-$config['proxy_ips'] = '';
+$config['index_page'] = "";
